b2a33d0e3a15a45a7241156c50ce2960aa67767b
miracle aigbogun <maigbogun881@gmail.com>
2024-02-01 20:13:59 -0600

    added window constructor that allows a window with a button and text to be made

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -1,5 +1,9 @@
 package Calculator;
+import CalculatorGUI.*;
+
 
 public class Calculator {
-
+    public static void main(String[] args) {
+        CalculatorGUI calculatorGUI = new CalculatorGUI("Java Calculator", 400, 300, true);
+    }
 }
--- src/CalculatorGUI/CalculatorGUI.java
+++ src/CalculatorGUI/CalculatorGUI.java
@@ -1,5 +1,32 @@
 package CalculatorGUI;
+import javax.swing.*;
+
+import java.awt.*;
+
+import static java.awt.SystemColor.text;
 
 public class CalculatorGUI {
+
+    public CalculatorGUI(String windowName, int windowWidth, int windowHeight, boolean setVisible){
+        JFrame jFrame = new JFrame(windowName);
+        jFrame.setSize(windowWidth, windowHeight);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Set the default close operation
+        JLabel jLabel = new JLabel("Some basic text");
+        jFrame.add(jLabel); // Add the label to the frame
+        JButton button = new JButton("test");
+        button.setSize(50,50);
+        jFrame.add(button);
+
+        jFrame.setVisible(setVisible); // Set the visibility after adding components
+        jFrame.pack();
+    }
+
+    public JButton addButton(String text){
+        JButton jButton = new JButton(text);
+        return jButton;
+    }
+
+
+
 
 }
