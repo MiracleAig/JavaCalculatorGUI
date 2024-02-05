477def44061bbc7384c87b85c82b2a12a827f8e3
miracle aigbogun <maigbogun881@gmail.com>
2024-02-04 19:44:57 -0600

    Updated README.md and CalculatorGUI with checking for proper input

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Dev branch of JavaCalculator any changes made here can and will be subject to change.
--Any code here might be unstable, so use with caution
+- Any code here might be unstable, so use with caution
+---
+# UPDATE 2/4/24
+-  Scraped the gui framework, this project is now going to be used fully in the console window
+---
--- src/CalculatorGUI/CalculatorGUI.java
+++ src/CalculatorGUI/CalculatorGUI.java
@@ -1,15 +1,50 @@
 package CalculatorGUI;
 
 
+import java.util.Scanner;
+
 public class CalculatorGUI {
 
+    private boolean check;
+    Scanner s = new Scanner(System.in);
     private MathOperator operators;
+    private int userSelection;
+
     public void ShowMenu(){
         System.out.println("--- Java Calculator ---");
         System.out.println("Options:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n**ALL VALUES MUST BE A DOUBLE**");
+        MakeSelection();
+        System.out.println();
+
+
+
     }
 
     public MathOperator getOperators() {return operators;}
+
+    public void ThrowError(){
+        System.out.println("Please enter a number 1 - 4 as a int.");
+        System.out.println();
+        MakeSelection();
+
+    }
+
+    public void processUserSelection(int userSelection) {
+
+    }
+
+
+
+    public void CheckSelection(int userInput){
+        if(userInput == 1 || userInput == 2 || userInput == 3 || userInput == 4){
+            // continue program
+        } else ThrowError();
+    }
+    public void MakeSelection(){
+        System.out.print("Make your selection now: ");
+        userSelection = s.nextInt();
+        CheckSelection(userSelection);
+    }
 
     public void getMathOperations(){
         switch (getOperators()){
